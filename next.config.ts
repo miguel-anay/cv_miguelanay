d42ed3c9f1c6977f214f6440b5c490bb3efb92a9
miguel anay <mba.miguel18@gmail.com>
2025-07-18 19:38:20 -0500

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
-
+const path = require('path'); // ✅ importa path de Node.js
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
